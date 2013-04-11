54353a5ec2cdb950484348341b9633bfd93d3b2a
Darby Frey <darby@electricshoes.com>
2013-04-11 14:56:15 -0500

    Added beacon_client

--- basic.rb
+++ basic.rb
@@ -1,5 +1,6 @@
 gem 'exception_notification', :git => 'git@github.com:darbyfrey/exception_notification.git'
 gem 'httparty'
+gem 'beacon_client', :git => '/Users/darby/projects/starterleague/beacon_client'
 
 inject_into_file 'config/environments/development.rb', :before => "end" do
   <<-eos
