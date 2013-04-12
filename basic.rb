33a1081a2273547ee271acc1b158394620ded842
Darby Frey <darby@electricshoes.com>
2013-04-12 15:53:59 -0500

    Added httparty to basic template

--- basic.rb
+++ basic.rb
@@ -1,6 +1,7 @@
 gem 'exception_notification', :git => 'git@github.com:darbyfrey/exception_notification.git'
 gem 'beacon_client', :git => '/Users/darby/projects/starterleague/beacon_client'
 gem 'starter_generators'
+gem 'httparty'
 
 inject_into_file 'config/environments/development.rb', :after => "config.assets.debug = true" do
   <<-eos
