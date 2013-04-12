8c3f2331d5750585ca66baa7b39ac18cceab4f36
Darby Frey <darby@electricshoes.com>
2013-04-12 15:47:04 -0500

    updated basic template

--- basic.rb
+++ basic.rb
@@ -5,6 +5,7 @@ gem 'starter_generators'
 inject_into_file 'config/environments/development.rb', :after => "config.assets.debug = true" do
   <<-eos
 
+
   # ExceptionNotifier gem configuration
   config.middleware.use ExceptionNotifier,
     :webhook => {
