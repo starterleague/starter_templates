5dbbae660cf39a59751203525c90bfe0c7906018
Darby Frey <darby@electricshoes.com>
2013-04-18 16:16:10 -0500

    Updated get.sh

--- get.sh
+++ get.sh
@@ -2,10 +2,10 @@
 
 curl -s https://raw.github.com/starterleague/starter_templates/master/.railsrc -o ~/.railsrc
 
-git clone git://github.com/starterleague/beacon_client.git /tmp/beacon_client
-cd /tmp/beacon_client
+git clone git://github.com/starterleague/beacon_client.git
+cd beacon_client
 bundle install
 rake build
 rake install
 cd
-rm -rf /tmp/beacon_client
+rm -rf beacon_client
